9f671d820c29e2c25afff97c9c1a98c6d726d584
rajagurunath <gurunathrajagopal@gmail.com>
2021-11-06 03:48:11 +0530

    renamed the folders

--- streamlit_react_flow/frontend/src/ReactFlowComponent.tsx
+++ streamlit_react_flow/frontend/src/ReactFlowComponent.tsx
@@ -16,7 +16,7 @@ interface State {
  * This is a React-based component template. The `render()` function is called
  * automatically when your component should be re-rendered.
  */
-class MyComponent extends StreamlitComponentBase<State> {
+class ReactFlowComponent extends StreamlitComponentBase<State> {
   public state = { numClicks: 0, isFocused: false }
 
   public render = (): ReactNode => {
